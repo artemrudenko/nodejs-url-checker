bc9e2c606a2866258b5ebab7df33634323310ce7
artemrudenko <artem_rudenko@list.ru>
2020-12-13 13:40:37 +0300

    add create check page

--- public/app.js
+++ public/app.js
@@ -62,7 +62,6 @@ app.client.request = function (headers, path, method, queryStringObject, payload
     if (xhr.readyState == XMLHttpRequest.DONE) {
       var statusCode = xhr.status;
       var responseReturned = xhr.responseText;
-
       // Callback if requested
       if (callback) {
         try {
@@ -74,11 +73,9 @@ app.client.request = function (headers, path, method, queryStringObject, payload
       }
     }
   }
-
   // Send the payload as JSON
   var payloadString = JSON.stringify(payload);
   xhr.send(payloadString);
-
 };
 
 // Bind the logout button
@@ -138,11 +135,28 @@ app.bindForms = function () {
         var elements = this.elements;
         for (var i = 0; i < elements.length; i++) {
           if (elements[i].type !== 'submit') {
-            var valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
-            if (elements[i].name == '_method') {
+            // Determine class of element and set value accordingly
+            var classOfElement = typeof (elements[i].classList.value) == 'string' && elements[i].classList.value.length > 0 ? elements[i].classList.value : '';
+            var valueOfElement = elements[i].type == 'checkbox' && classOfElement.indexOf('multiselect') == -1 ? elements[i].checked : classOfElement.indexOf('intval') == -1 ? elements[i].value : parseInt(elements[i].value);
+            var elementIsChecked = elements[i].checked;
+            // Override the method of the form if the input's name is _method
+            var nameOfElement = elements[i].name;
+            if (nameOfElement == '_method') {
               method = valueOfElement;
             } else {
-              payload[elements[i].name] = valueOfElement;
+              // Create an payload field named "method" if the elements name is actually httpmethod
+              if (nameOfElement == 'httpmethod') {
+                nameOfElement = 'method';
+              }
+              // If the element has the class "multiselect" add its value(s) as array elements
+              if (classOfElement.indexOf('multiselect') > -1) {
+                if (elementIsChecked) {
+                  payload[nameOfElement] = typeof (payload[nameOfElement]) == 'object' && payload[nameOfElement] instanceof Array ? payload[nameOfElement] : [];
+                  payload[nameOfElement].push(valueOfElement);
+                }
+              } else {
+                payload[nameOfElement] = valueOfElement;
+              }
             }
           }
         }
@@ -214,6 +228,10 @@ app.formResponseProcessor = function (formId, requestPayload, responsePayload) {
   if (formId == 'accountEdit3') {
     app.logUserOut(false);
     window.location = '/account/deleted';
+  }
+  // If the user just created a new check successfully, redirect back to the dashboard
+  if (formId == 'checksCreate') {
+    window.location = '/checks/all';
   }
 };
 
