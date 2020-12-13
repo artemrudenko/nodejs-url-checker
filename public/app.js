c8913ee9e37fc357b66552f09160649a744c8849
artemrudenko <artem_rudenko@list.ru>
2020-12-13 13:13:30 +0300

    add delete account

--- public/app.js
+++ public/app.js
@@ -71,7 +71,6 @@ app.client.request = function (headers, path, method, queryStringObject, payload
         } catch (e) {
           callback(statusCode, false);
         }
-
       }
     }
   }
@@ -93,7 +92,9 @@ app.bindLogoutButton = function () {
 };
 
 // Log the user out then redirect them
-app.logUserOut = function () {
+app.logUserOut = function (redirectUser) {
+  // Set redirectUser to default to true
+  redirectUser = typeof (redirectUser) == 'boolean' ? redirectUser : true;
   // Get the current token id
   var tokenId = typeof (app.config.sessionToken.id) == 'string' ? app.config.sessionToken.id : false;
 
@@ -105,7 +106,9 @@ app.logUserOut = function () {
     // Set the app.config token as false
     app.setSessionToken(false);
     // Send the user to the logged out page
-    window.location = '/session/deleted';
+    if (redirectUser) {
+      window.location = '/session/deleted';
+    }
   });
 };
 
@@ -144,8 +147,11 @@ app.bindForms = function () {
           }
         }
 
+        // If the method is DELETE, the payload should be a queryStringObject instead
+        var queryStringObject = method == 'DELETE' ? payload : {};
+
         // Call the API
-        app.client.request(undefined, path, method, undefined, payload, function (statusCode, responsePayload) {
+        app.client.request(undefined, path, method, queryStringObject, payload, function (statusCode, responsePayload) {
           // Display an error on the form if needed
           if (statusCode !== 200) {
             if (statusCode == 403) {
@@ -203,6 +209,11 @@ app.formResponseProcessor = function (formId, requestPayload, responsePayload) {
   var formsWithSuccessMessages = ['accountEdit1', 'accountEdit2'];
   if (formsWithSuccessMessages.indexOf(formId) > -1) {
     document.querySelector("#" + formId + " .formSuccess").style.display = 'block';
+  }
+  // If the user just deleted their account, redirect them to the account-delete page
+  if (formId == 'accountEdit3') {
+    app.logUserOut(false);
+    window.location = '/account/deleted';
   }
 };
 
